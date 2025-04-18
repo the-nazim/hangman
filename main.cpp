a8dde30eece54e0950b3a54e6948f3fcb379487e
the-nazim <thenazim0707@gmail.com>
2025-04-18 17:46:15 +0000

    Foundation code

--- main.cpp
+++ main.cpp
@@ -28,25 +28,45 @@ int main()
     char guess;
     map<char, int> letters;
     map<char, int> guess_letter;
+    vector<char> vect_char;
+    vector<char> vect_res;
     srand(time(0));
     int random_index = rand() % word_list.size();
     string secret_word = word_list[random_index];
     cout<<"Word: "<<secret_word<<endl;
 
     for(int i=0; i<secret_word.length(); i++)
-    {
         letters[secret_word[i]]++;
-    }
 
+    for(int i=0; i<secret_word.length(); i++)
+    {    
+        vect_char.push_back(secret_word[i]);
+        vect_res.push_back('_');
+    }
     while(attempt<6)
     {
         cout<<"Guess the letter: ";
         cin>>guess;
+        guess_word = "";
         guess_letter[guess]++;
         if((secret_word.find(guess) != string::npos) && (letters[guess]>=guess_letter[guess]))
         {
             cout<<"Correct guess!"<<endl;
-            guess_word += guess;
+            for(int i=0; i<vect_char.size(); i++)
+            {
+                if((vect_char[i] == guess) && (vect_res[i] == '_'))
+                {
+                    vect_res[i] = vect_char[i];
+                    break;
+                }
+            }
+            for(auto i: vect_res)
+            {
+                cout<<i<<" ";
+                if(i!='_')
+                    guess_word += i;   
+            }
+            cout<<endl;
             cout<<guess_word<<endl;
             if(secret_word == guess_word)
             {
