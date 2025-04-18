b3d24d8add020f427edf7760b94efeea87375bff
the-nazim <thenazim0707@gmail.com>
2025-04-18 16:10:15 +0000

    Second commit

--- main.cpp
+++ main.cpp
@@ -19,31 +19,44 @@ int main()
     vector<string> word_list;
 
     while (file >> word)
-    {
         word_list.push_back(word);
-    }
+
     file.close();
+    
+    int attempt = 0;
+    string guess_word = "";
+    char guess;
+    map<char, int> letters;
+    map<char, int> guess_letter;
     srand(time(0));
     int random_index = rand() % word_list.size();
-    // cout<<"Random word: "<<word_list[random_index]<<endl;
     string secret_word = word_list[random_index];
     cout<<"Word: "<<secret_word<<endl;
-    int attempt = 0;
-    string guess = "";
-    string guess_word = "";
+
+    for(int i=0; i<secret_word.length(); i++)
+    {
+        letters[secret_word[i]]++;
+    }
+
     while(attempt<6)
     {
         cout<<"Guess the letter: ";
         cin>>guess;
-        if(secret_word.find(guess) != string::npos)
+        guess_letter[guess]++;
+        if((secret_word.find(guess) != string::npos) && (letters[guess]>=guess_letter[guess]))
         {
             cout<<"Correct guess!"<<endl;
             guess_word += guess;
+            cout<<guess_word<<endl;
             if(secret_word == guess_word)
             {
                 cout<<"You win!"<<endl;
                 break;
             }
+        }
+        else if((secret_word.find(guess) != string::npos) && (letters[guess]<guess_letter[guess]))
+        {
+            cout<<"Already guessed!"<<endl;
         }
         else
         {
@@ -51,5 +64,4 @@ int main()
             attempt++;
         }
     }
-
 }
